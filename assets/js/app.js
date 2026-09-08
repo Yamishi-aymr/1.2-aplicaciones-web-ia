5133e9399328f31d3974ac79fcbc0026cc8011c4
Alicia0420 <alicia.mariano0420@gmail.com>
2026-09-07 20:51:16 -0600

    Actualiza URL del backend2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const API_URL =
-    "https://1-2-aplicaciones-web-1ah1p4zbd-alice-70fd.vercel.app/api/chat";
+    "https://1-2-aplicaciones-web-ia-git-main-alice-70fd.vercel.app/api/chat";
 
 const form = document.getElementById("chatForm");
 const input = document.getElementById("messageInput");
